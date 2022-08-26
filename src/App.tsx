2d017b6eb601973d69af4cbcfa60883284baecbd
Alex-Archer-I <71606955+Alex-Archer-I@users.noreply.github.com>
2022-08-26 07:40:35 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,20 +19,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-<a href="https://www.flaticon.com/free-icons/scientist" title="scientist icons">Scientist icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/boy" title="boy icons">Boy icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/cool" title="cool icons">Cool icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/woman" title="woman icons">Woman icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/girl" title="girl icons">Girl icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/artist" title="artist icons">Artist icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/skater" title="skater icons">Skater icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/flower" title="flower icons">Flower icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/man" title="man icons">Man icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/halloween" title="halloween icons">Halloween icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/king" title="king icons">King icons created by Freepik - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/followers" title="followers icons">Followers icons created by SBTS2018 - Flaticon</a>
-<a href="https://www.flaticon.com/free-icons/remove" title="remove icons">Remove icons created by Phoenix Group - Flaticon</a> 
- */
